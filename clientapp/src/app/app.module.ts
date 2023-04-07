0ff0d00af579181b8d4554109267e9960f9867e8
hanyousystem <hanyousystem@nscd.nstac.go.jp129143288+hanyousystem@users.noreply.github.com>
2023-04-07 13:20:01 +0900

    最新版

--- clientapp/src/app/app.module.ts
+++ clientapp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { SystemComponent_Gaisei } from './system/system.component_Gaisei';
 import { ShowSystemComponent_Gaisei } from './system/show-system/show-system.component_Gaisei';
+import { ROUTES, RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,6 @@ import { ShowSystemComponent_Gaisei } from './system/show-system/show-system.com
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatTableModule
-
   ],
   providers: [ApiserviceService],
   bootstrap: [AppComponent]
