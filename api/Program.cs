9d5c0f7c148a24d551c70cbfc1027a4c6a46a920
hidekazunasu <hnasu@nscd.nstac.go.jp>
2023-06-16 20:21:50 +0900

    ヘッダーの固定と枠線の設定

--- api/Program.cs
+++ api/Program.cs
@@ -12,11 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context to DI container using SQLite database.
 builder.Services.AddDbContext<inHouseDbContext>(options =>
 {
-    // var path = builder.Configuration.GetConnectionString("inHouseDbConnection");
-    // options.UseSqlite(path);
-    var path = builder.Configuration.GetConnectionString("SQLConnection");
-    options.UseSqlServer(path);
-    inHouseDbContext.ConfigPath =path;
+    var path = builder.Configuration.GetConnectionString("inHouseDbConnection");
+    options.UseSqlite(path);
+    //var path = builder.Configuration.GetConnectionString("SQLConnection");
+    //options.UseSqlServer(path);
+    inHouseDbContext.ConfigPath = path;
 });
 builder.Services.AddLogging(options => options.AddConsole());
 //appsetting.jsonを設定
